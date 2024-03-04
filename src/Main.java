c49108958878b54a80cec247029f0e3a45625fe8
Ahmed Dogan <114875634+MuhammedAhmedDogan@users.noreply.github.com>
2024-03-05 01:12:59 +0300

    v4

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int row = 2, col = 2;
+        int row = 2, col = 2; // Kullanıcı değer girmeden önce tanımlanmış tahta boyutlarının sınır değerleri.
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Mayın Tarlasına Hoşgeldiniz. Oyun Tahtanızın Boyutunu Ayarlayalım");
+        // Matris boyutunu belirlemek için kullanıcıdan veri alındı. ( Değerlendirme formu 7 )
+        System.out.println("Mayın Tarlası Oyununa Hoşgeldiniz.\nOyun Tahtanızın Boyutunu Ayarlayalım");
         do {
             if (row < 2 || col < 2) {
                 System.out.println("====================");
                 System.out.println("Satır veya sütün sayısı 2'den küçük olamaz! Lütfen tekrar giriniz :");
             }
-            System.out.print("Satır sayısı :");
+            System.out.print("Satır sayısı : ");
             row = scan.nextInt();
-            System.out.print("Sütun sayısı :");
+            System.out.print("Sütun sayısı : ");
             col = scan.nextInt();
         } while (row < 2 || col < 2);
 
+        // MineSweeper sınıfından mineSweeper nesnesi üretilerek oyun başlatıldı.
         MineSweeper mineSweeper = new MineSweeper(row, col);
         mineSweeper.run();
-
     }
 }
--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -2,51 +2,50 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class MineSweeper {
-    // Değişkenler
-    int row, col, size, mines, winCount;
-    boolean isWin = false, isLose = false;
-    String[][] board, map;
-    Random random = new Random();
-    Scanner scan = new Scanner(System.in);
+public class MineSweeper { // Değerlendirme formu 5
+    // Değişkenler. MineSweeper sınıfının nitelikleri
+    int row, col, size, mines, winCount; // Oyun tahtasının boyutları, mayın sayısı ve kazanma durumunu hesaplamak bir sayaç tanımlandı.
+    boolean isWin = false, isLose = false; // Oyunun kazanıldığını ya da kaybedildiğini belirleyen boolean değişkenler.
+    String[][] board, map; // Kullanıcıya gösterilen oyun tahtası ve oyun boyunca hesaplamalar için kullanılan, oyun sonunda gösterilen mayın konumları.
+    Random random = new Random(); // Mayın konumlarını rastgele belirlemek için Random sınıfından bir random nesnesi oluşturuldu.
+    Scanner scan = new Scanner(System.in); // Kullanıcıdan veri almak için Scanner sınıfından scan nesnesi oluşturuldu.
 
+    // MineSweeper sınıfının constructor metodu. Kullanıcıdan alınan satır ve sütun değerleri ile oyun tahtasının niteliklerini oluşturur.
     MineSweeper(int row, int col) {
         this.row = row;
         this.col = col;
         this.size = row * col;
-        this.mines = (int) Math.round(this.size / 4.0);
+        this.mines = (int) Math.round(this.size / 4.0); // Tahta büyüklüğüne göre mayın sayısı hesaplandı.
         this.board = new String[row][col];
         this.map = new String[row][col];
     }
 
+    // Oyunu çalıştıran run metodu. ( Değerlendirme formu 6 )
     public void run() {
         this.boardCreator(this.board);
         this.boardCreator(this.map);
         this.mineCreator();
 
+        // Değerlendirme formu 11
         while (!isWin && !isLose) {
             this.print(this.board);
-            this.print(this.map);
             this.select();
         }
-
-        if (isWin) {
-            this.print(map);
-            System.out.println("Tebrikler oyunu kazandınız.");
-        }
-
-        if (isLose) {
-            this.print(map);
-            System.out.println("Oyun bitti. Mayına bastınız !");
-        }
+        // Değerlendirme formu 15
+        System.out.println("Mayınların konumu");
+        this.print(map);
+        String conclusion = isWin ? "Tebrikler oyunu kazandınız." : "Oyun bitti. Mayına bastınız !";
+        System.out.println(conclusion);
     }
 
+    // Boş oyun tahtasını oluşturan boardCreator metodu.
     public void boardCreator(String[][] arr) {
         for (String[] strings : arr) {
             Arrays.fill(strings, "-");
         }
     }
 
+    // Tahtanın 1/4'ü sayıda mayın üretip rastgele yerleştiren mineCreator metodu. (Değerlendirme formu 8 )
     public void mineCreator() {
         int randRow, randCol, count = 0;
         while (count < this.mines) {
@@ -59,6 +58,7 @@ public class MineSweeper {
         }
     }
 
+    // Oyun tahtasını ekrana bastıran print metodu.
     public void print(String[][] arr) {
         for (String[] row : arr) {
             for (String col : row) {
@@ -69,30 +69,33 @@ public class MineSweeper {
         System.out.println("====================");
     }
 
+    // Kullanıcıdan işaretlemek istediği koordinatları alan ve uygunluğunu kontrol eden select metodu. ( Değerlendirme formu 9, 10 )
     public void select() {
         for (; ; ) {
-            System.out.println("Tahtada açmak istediğiniz koordinatları satır ve sütun olarak giriniz.");
+            System.out.println("Tahtada açmak istediğiniz koordinatları satır ve sütun olarak giriniz.");    // Değerlendirme formu 9
             System.out.print("Satır :");
             int selectedRow = scan.nextInt();
             System.out.print("Sütun :");
             int selectedCol = scan.nextInt();
 
-            if (selectedRow < 0 || selectedCol < 0 || selectedRow >= this.row || selectedCol >= this.col) {
+            if (selectedRow < 0 || selectedCol < 0 || selectedRow >= this.row || selectedCol >= this.col) { // Değerlendirme formu 10
                 System.out.println("Oyun tahtası dışında bir koordinat girdiniz ! Lütfen tekrar giriniz.");
-            } else if (this.map[selectedRow][selectedCol].equals("*")) {
+            } else if (this.map[selectedRow][selectedCol].equals("*")) {                                    // Değerlendirme formu 13 ( Mayına basılma durumu )
                 this.isLose = true;
                 break;
             } else if (!this.board[selectedRow][selectedCol].equals("-") && !this.map[selectedRow][selectedCol].equals("*")) {
                 System.out.println("Daha önce seçilmiş bir koordinat girdiniz ! Lütfen tekrar giriniz.");
             } else {
                 this.countMines(selectedRow, selectedCol);
-                if (++winCount == (this.size - this.mines))
+                if (++winCount == (this.size - this.mines)) // Değerlendirme formu 14 ( Oyunu kazanma durumu )
                     this.isWin = true;
                 break;
             }
         }
     }
 
+    // Girilen noktanın çevresindeki mayınları sayan countMines metodu. ( Değerlendirme formu 12 )
+    // select metodu içerisinde seçilen noktada mayın olmaması durumunda çağırıldı.
     public void countMines(int row, int col) {
         int count = 0;
         for (int i = row - 1; i <= row + 1; i++) {
@@ -109,6 +112,5 @@ public class MineSweeper {
         this.board[row][col] = Integer.toString(count);
         this.map[row][col] = Integer.toString(count);
     }
-
 
 }
